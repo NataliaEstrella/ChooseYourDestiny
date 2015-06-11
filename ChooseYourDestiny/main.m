71aaf1d767033cd53fafcc2b8e1291ddc0df2d80
Natalia Estrella <natalia.c.estrella@gmail.com>
2015-06-11 00:24:49 -0400

    added grade checks

--- ChooseYourDestiny/main.m
+++ ChooseYourDestiny/main.m
@@ -8,29 +8,42 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        
         
         char name[256];
+        int grade;
+        char next[256];
         
-    
+        
         
         NSLog(@"Hello, Whats your name?");
-        
         scanf("%s", &name);
-        
         NSLog(@"Hi, nice to meet you %@!", @(name));
         
+        //       This weird next function is so my Logs arent shooting out all at once... This is my nect button. Imagine there is
+        scanf("%s", &next);
+        NSLog(@"...%s",next);
+//        NSLog()
+        
+        NSLog(@"What grade are you in?");
         
         
-    
+        scanf("%i", &grade);
         
+        NSLog(@"Oh cool %i", grade);
         
-        
-        
-        
-    
+        if (grade <=6)
+        {
+            NSLog(@"Talk about stolen something mystery");
+        }
+        else
+        {
+            NSLog(@"Talk about the weird old man that lives in the woods");
+        }
         
     }
     return 0;

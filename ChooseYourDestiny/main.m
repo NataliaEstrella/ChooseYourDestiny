49ba5c4da30deef205cb0d6075b71d441c41188b
Natalia Estrella <natalia.c.estrella@gmail.com>
2015-06-11 15:51:56 -0400

    With sentence = nil; About to change to NSLog

--- ChooseYourDestiny/main.m
+++ ChooseYourDestiny/main.m
@@ -18,33 +18,44 @@ int main(int argc, const char * argv[])
         int grade;
         char next[256];
         
+        BOOL knowsAlien;
+        int alien;
+        
         
         
         NSLog(@"Hello, Whats your name?");
         scanf("%s", &name);
-        NSLog(@"Hi, nice to meet you %@!", @(name));
+        NSLog(@"Hi, nice to meet you %@!, welcome to Turd Academy", @(name));
         
-        //       This weird next function is so my Logs arent shooting out all at once... This is my nect button. Imagine there is
-        scanf("%s", &next);
-        NSLog(@"...%s",next);
+//       This weird next function is so my Logs arent shooting out all at once... This is my nect button. Imagine there is
+//        NSLog(@"Type next when ready%s",next);
+//        scanf("%s", &next);
 //        NSLog()
         
         NSLog(@"What grade are you in?");
         
         
         scanf("%i", &grade);
+        NSLog(@"Oh cool. Grade %i, that's dope", grade);
         
-        NSLog(@"Oh cool %i", grade);
+        NSString *sentence = nil;
         
-        if (grade <=6)
+        if (grade <= 6)
         {
-            NSLog(@"Talk about stolen something mystery");
+            sentence = @" Have ave you heard about the aliens that live in the abandoned pool?";
+            scanf("%d", &alien);
+            if (knowsAlien) {
+                NSLog(@"Oh, cool. So probably know all the teachers are aliens too that ar trying to go back to their home planet.");
+            } else {
+                NSLog(@"No?... Oh, haha just kidding.");
+            }
         }
         else
         {
-            NSLog(@"Talk about the weird old man that lives in the woods");
+            sentence = @"Talk about the weird old man that lives in the woods";
         }
         
+        NSLog(@"%@", sentence);
     }
     return 0;
 }
